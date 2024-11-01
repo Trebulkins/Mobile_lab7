f4d9e3eee401487764032fd33d1708ca5afe03ab
Trebulkins <trebinivan2005@gmail.com>
2024-11-01 15:26:18 +0700

    6.1 ver. 2

--- app/src/main/java/com/example/mobile_lab7/Criminal_info.kt
+++ app/src/main/java/com/example/mobile_lab7/Criminal_info.kt
@@ -1,30 +1,20 @@
 package com.example.mobile_lab7
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Criminal_info : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_criminal_info)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val addBut = findViewById<ImageButton>(R.id.addButton)
-
-        addBut.setOnClickListener {
-            val aCrime = Intent(this@MainActivity, Criminal_info::class.java)
-            aCrime.putExtra("Move", "add")
-            startActivity(aCrime)
         }
     }
 }
