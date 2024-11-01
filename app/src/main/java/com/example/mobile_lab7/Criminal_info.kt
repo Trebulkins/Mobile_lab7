aa58c02537eceba7bdf8ecbc893fc4e1f6b15c83
Trebulkins <trebinivan2005@gmail.com>
2024-11-01 16:06:47 +0700

    6.1 ver. 3

--- app/src/main/java/com/example/mobile_lab7/Criminal_info.kt
+++ app/src/main/java/com/example/mobile_lab7/Criminal_info.kt
@@ -1,10 +1,13 @@
 package com.example.mobile_lab7
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
 
 class Criminal_info : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,26 @@ class Criminal_info : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val datePick = findViewById<Button>(R.id.ChooseDate)
+
+        datePick.setOnClickListener {
+            val c = Calendar.getInstance()
+
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this,
+                { view, year, monthOfYear, dayOfMonth -> datePick.text =
+                    (dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year)
+                },
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
         }
     }
 }
